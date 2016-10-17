67bf054e62e308cae6d92995c6411cf94e4d0585
David <dyz2102@gmail.com>
2016-10-17 19:57:05 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -46,6 +46,32 @@ class GroupsController < ApplicationController
     redirect_to groups_path
   end
 
+  def join
+    @group = Group.find(params[:id])
+
+      if !current_user.is_member_of?(@group)
+        current_user.join!(@group)
+        flash[:notice] = "Joined Discussion Successfully!"
+      else
+        flash[:warning] = "Already Member of This Discussion!"
+      end
+
+      redirect_to group_path(@group)
+    end
+
+  def quit
+      @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "Quit Discussion Successfully!"
+    else
+      flash[:warning] = "Not Currently Member"
+    end
+
+    redirect_to group_path(@group)
+  end
+
   private
 
   def find_group_and_check_permission
